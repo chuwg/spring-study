bcb858d24b4dec4b76c0585c6b32f88f024312ce
chuwg <gi828282@naver.com>
2022-06-23 16:11:59 +0900

    정적 페이지 생성 확인용

--- src/main/java/hellospring/hello/controller/HelloController.java
+++ src/main/java/hellospring/hello/controller/HelloController.java
@@ -3,6 +3,7 @@ package hellospring.hello.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
